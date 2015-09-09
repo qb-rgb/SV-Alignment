d4bdaa56be55d4da68ccb7637424f50c76e57644
qw0te <baert.quentin@gmail.com>
2015-09-09 15:39:56 +0200

    Fait de Aligner une classe abstraite

--- src/main/scala/alignment/Aligner.scala
+++ src/main/scala/alignment/Aligner.scala
@@ -3,7 +3,7 @@
   *
   * @author Quentin Baert
   */
-class Aligner(
+abstract class Aligner(
   val sequence1: String,
   val sequence2: String,
   val matchScore: Int,
@@ -12,12 +12,12 @@ class Aligner(
   val brink: Int
 ) {
 
-  private val n1: Int = sequence1.length
-  private val n2: Int = sequence2.length
+  protected val n1: Int = sequence1.length
+  protected val n2: Int = sequence2.length
 
   assert(n1 >= n2)
 
-  private def cost(array: Array[Array[Int]], i: Int, j: Int, char1: Char, char2: Char): Int = {
+  protected def cost(array: Array[Array[Int]], i: Int, j: Int, char1: Char, char2: Char): Int = {
     def max3(x: Int, y: Int, z: Int): Int =
       math.max(x, math.max(y, z))
 
@@ -32,24 +32,6 @@ class Aligner(
     max3(matchOrMisMatch, indel1, indel2)
   }
 
-  def align: Array[Array[Int]] = {
-    val dim1 = this.n1 + 1
-    val dim2 = this.n2 + 1
-    val scores = Array.ofDim[Int](dim1, dim2)
-
-    for (i <- 0 until dim1)
-      scores(i)(0) = -i
-
-    for (j <- 0 until dim2)
-      scores(0)(j) = -j
-
-    for (i <- 1 until dim1) {
-      for (j <- 1 until dim2) {
-        scores(i)(j) = this.cost(scores, i, j, this.sequence1(i - 1), this.sequence2(j - 1))
-      }
-    }
-
-    scores
-  }
+  def align: Array[Array[Int]]
 
 }
