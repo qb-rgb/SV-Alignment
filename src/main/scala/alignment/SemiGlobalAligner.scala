86affbdd27d242a7f11eeec6c3df5c082f81ed18
qw0te <baert.quentin@gmail.com>
2015-09-15 23:52:40 +0200

    Modifie la fonction SemiGlobalAligner.showAlignment() afin qu'elle prenne en compte le bandwidth

--- src/main/scala/alignment/SemiGlobalAligner.scala
+++ src/main/scala/alignment/SemiGlobalAligner.scala
@@ -21,18 +21,26 @@ class SemiGlobalAligner(
   /** @see alignment.Aligner.align() */
   override protected def align: Array[Array[Int]] = {
     val scores = Array.ofDim[Int](this.n2 + 1, this.n1 + 1)
+    val minusInfinity = -100
 
     // First row
-    for (j <- 0 to this.n1)
+    for (j <- 0 to this.error)
       scores(0)(j) = 0
 
     // First column
-    for (i <- 1 to this.n2)
+    for (i <- 1 to this.error)
       scores(i)(0) = i * indelScore
 
-    // Rest of the matrix
+    // Diagonals
+    val diag1Couples = (this.error + 1 to this.n2) zip (0 to this.error)
+    val diag2Couples = (0 to this.n2) zip (this.error + 1 to this.n1)
+
+    for ((i, j) <- diag1Couples) scores(i)(j) = minusInfinity
+    for ((i, j) <- diag2Couples) scores(i)(j) = minusInfinity
+
+    // Rest of the matrix in the band of width k where k is the error of the aligner
     for (i <- 1 to this.n2)
-      for (j <- 1 to this.n1)
+      for (j <- math.max(1, i - this.error) to math.min(i + this.error, this.n1))
         scores(i)(j) = this.cost(scores, i, j, this.sequence1(j - 1), this.sequence2(i - 1))
 
     scores
