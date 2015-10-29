cea1e947ad46ab43b36592a5c5dd86f3906a84ec
qw0te <baert.quentin@gmail.com>
2015-10-29 17:05:45 +0100

    Corrige les méthodes de construction et de recherche dans la table des suffixes

--- src/main/scala/indexation/indexation.scala
+++ src/main/scala/indexation/indexation.scala
@@ -2,40 +2,49 @@ package object indexation {
 
   type SuffixTable = List[Int]
 
-  /**
-    * Build the suffix table from a genome.
-    *
-    * @param genome genome from which build the suffix table
-    * @return suffix table of the genome
-    */
-  /*
-  def buildSuffixTableFrom(genome: String): SuffixTable = {
-    val n = genome.length
-    (0 until genome.length).toList sortWith {
-      (i, j) => genome.substring(i) < genome.substring(j)
-    }
-  }
-  */
-
-  def f(genome: String, iinit: Int, jinit: Int, i: Int, j: Int): Boolean = {
-    if (i >= genome.length || j >= genome.length)
-      iinit + i < jinit + j
+  // Compare two section of the genome without using the substring method
+  private def indexCompare(
+    genome: String,
+    iinit: Int,
+    jinit: Int,
+    i: Int,
+    j: Int
+  ): Boolean =
+    if (j >= genome.length || i >= genome.length)
+      i > j
     else {
       val gi = genome(i)
       val gj = genome(j)
 
       if (gi == gj)
-        f(genome, iinit, jinit, i + 1, j + 1)
+        indexCompare(genome, iinit, jinit, i + 1, j + 1)
       else
         gi < gj
     }
-  }
 
-  def buildSuffixTableFrom(genome: String): SuffixTable = {
+    // startsWith method without copying the strings
+  private def indexStartsWith(
+    genome: String,
+    read: String,
+    gi: Int,
+    ri: Int
+  ): Boolean =
+    if (ri >= read.length)
+      true
+    else if (gi >= genome.length || genome(gi) != read(ri))
+      false
+    else
+      indexStartsWith(genome, read, gi + 1, ri + 1)
+
+  /** Build the suffix table from a genome.
+    *
+    * @param genome genome from which build the suffix table
+    * @return suffix table of the genome
+    */
+  def buildSuffixTableFrom(genome: String): SuffixTable =
     (0 until genome.length).toList sortWith {
-      (i, j) => f(genome, i, j, i, j)
+      (i, j) => indexCompare(genome, i, j, i, j)
     }
-  }
 
   /** Make a binary search to get the indexes of a read in the suffix table.
     *
@@ -57,32 +66,34 @@ package object indexation {
       else {
         val next = f(i)
 
-        if (genome.substring(st(next)) startsWith read)
+        if (indexStartsWith(genome, read, st(next), 0))
           getBorder(next, cond, f, st)
         else
           i
       }
 
     // Make a binary search to find the interval of indexes
-    def binarySearch(suffixTable: SuffixTable): Option[(Int, Int)] = {
+    def binarySearch(suffixTable: SuffixTable, i: Int): Option[(Int, Int)] = {
       val n = suffixTable.length
       val m = n / 2
+      // Remplacer le substring par un drop
       val w = genome.substring(suffixTable(m))
 
       if (suffixTable.isEmpty)
         None
-      else if (w startsWith read)
+      // else if (w startsWith read)
+      else if (indexStartsWith(genome, read, suffixTable(m), 0))
         Some((
-          getBorder(m, { _ == 0 }, { _ - 1 }, suffixTable),
-          getBorder(m, { _ == n - 1 }, { _ + 1 }, suffixTable)
+          i + getBorder(m, { _ == 0 }, { _ - 1 }, suffixTable),
+          i + getBorder(m, { _ == n - 1 }, { _ + 1 }, suffixTable)
         ))
       else if (w < read)
-        binarySearch(suffixTable drop m)
+        binarySearch(suffixTable drop m, i + m)
       else
-        binarySearch(suffixTable take m)
+        binarySearch(suffixTable take m, i)
     }
 
-    binarySearch(suffixTable)
+    binarySearch(suffixTable, 0)
   }
 
 }
