c786db326fc8a77a7fe37ece833c82f0aa45c532
qw0te <baert.quentin@gmail.com>
2015-10-30 12:44:59 +0100

    Ajoute une classe abstraite Indexer pour représenter les objets qui indexent un génome

---
+++ src/main/scala/indexation/Indexer.scala
@@ -0,0 +1,13 @@
+package indexation
+
+/** Represent a indexer. An indexer index a genome. */
+trait Indexer {
+
+  /** Find the locations of a sequence in the index genome.
+    *
+    * @param sequence sequence to find
+    * @return iterator of indexes of the sequence in the genome
+    */
+  def getSequenceLocation(sequence: String): Iterator[Int]
+
+}
