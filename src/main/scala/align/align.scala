2eb56012cfd973200a4fe85fcde39d2c88eee619
qw0te <baert.quentin@gmail.com>
2015-11-02 19:30:40 +0100

    Ajoute une fonction countNucs qui compte les nucléotides d'une séquence au package align

--- src/main/scala/align/align.scala
+++ src/main/scala/align/align.scala
@@ -1,1 +1,11 @@
-package object align {}
+package object align {
+
+  /** Count the number of nucleotides in a sequence.
+    *
+    * @param sequence sequence in which count the nucleotides
+    * @return number of nucleotides in the sequence
+    */
+  def countNucs(sequence: String): Int =
+    (sequence filterNot { _ == '-' }).length
+
+}
